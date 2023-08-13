c7828d39f6cd2e170a4ed4232b95ebf8802f8571
Marcus Dashe <marcussdashe.developer@gmail.com>
2023-08-13 02:06:14 -0700

    Done implementing Responsive Nav Bar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com"], // add your domain here if
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
